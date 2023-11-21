6b1058e9daca94d64c3d7c30909065b670c84b94
KhalidNazzar <75658324+KhalidNazzar@users.noreply.github.com>
2023-11-21 12:02:39 +0400

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,47 @@
+from transformers import BlipProcessor, BlipForConditionalGeneration, DetrImageProcessor, DetrForObjectDetection
+from PIL import Image
+import torch
+import requests
+
+def get_image_caption(image_path):
+    image = Image.open(image_path).convert('RGB')
+
+    model_name = 'Salesforce/blip-image-captioning-large'
+
+    device = "cpu"
+
+    processor = BlipProcessor.from_pretrained(model_name)
+    model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
+
+    inputs = processor(image, return_tensors='pt').to(device)
+    output = model.generate(**inputs, max_new_tokens=20)
+
+    caption = processor.decode(output[0], skip_special_tokens=True)
+
+    return caption
+
+def detect_objects(image_path):
+    image = Image.open(image_path).convert('RGB')
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+    detections = ""
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        detections += '[{}, {}, {}, {}]'.format(int(box[0]), int(box[1]), int(box[2]), int(box[3]))
+        detections += ' {}'.format(model.config.id2label[int(label)])
+        detections += ' {}'.format(float(score))
+
+    return detections
+
+
+
+if __name__ == "__main__":
+    image_path = "/home/khalid/Music/Khalid_photo.jpeg"
+    detections = detect_objects(image_path)
+    print(detections)
---
+++ main.py
@@ -0,0 +1,55 @@
+    # openai_api_key="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",
+from tempfile import NamedTemporaryFile
+import streamlit as st
+from langchain.agents import initialize_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from tools_1 import ImageCaptionTool, ObjectDetectionTool
+from PIL import Image
+
+# Initialize tools only once
+tools = [ImageCaptionTool(), ObjectDetectionTool()]
+
+conversational_memory = ConversationBufferWindowMemory(
+    memory_key='chat_history',
+    k=1,
+    return_messages=True
+)
+
+llm = ChatOpenAI(
+    openai_api_key="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",
+    temperature=0.5,
+    model_name="gpt-3.5-turbo"
+)
+
+agent = initialize_agent(
+    agent="structured-chat-zero-shot-react-description",
+    tools=tools,
+    llm=llm,
+    max_iterations=5,
+    verbose=True,
+    memory=conversational_memory
+)
+
+st.title('Ask a question to image')
+st.header('Please upload an image')
+file = st.file_uploader("Upload an Image", type=["jpeg", "jpg", "png"])
+
+if file:
+    st.image(file, use_column_width=True)
+
+    user_question = st.text_input('Ask question about image: ')
+
+    if user_question and user_question.strip():
+        with NamedTemporaryFile(dir='.') as f:
+            f.write(file.getbuffer())
+            image_path = f.name
+
+            # Resize image if needed (optional)
+            # image = Image.open(image_path)
+            # image = image.resize((desired_width, desired_height))
+            # image.save(image_path)
+
+            with st.spinner(text='In progress...'):
+                response = agent.run(f'{user_question}, this is the image path: {image_path}')
+                st.write(response)
